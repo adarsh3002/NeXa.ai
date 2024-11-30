36ee64685505395ad0f76c41a9ae75665b49462f
adarsh3002 <ak47kumaradarsh@gmail.com>
2024-11-30 16:20:43 +0530

    first push

---
+++ server/routes/userDashboardRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { userDetails } = require("../controllers/userDashboard");
+
+router.route("/details").post(userDetails);
+// router.route("/user").get(details)
+
+
+module.exports = router;
